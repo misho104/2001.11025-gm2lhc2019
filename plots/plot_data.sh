1bf11635a689ba2a94069b7aa81acff54553808a
Sho Iwamoto <contact@misho-web.com>
2019-08-31 13:58:47 +0200

    fix tau-factor in linewise analysis of tab1-0.50

--- plots/plot_data.sh
+++ plots/plot_data.sh
@@ -3,4 +3,5 @@
 wolframscript -script plot_data.wls ../spectrum/x050/tab1_*.slha
 mv plot_data_masses.pdf plot_data_tab1_x050.pdf
 mv plot_data_cfactors.pdf plot_data_tab1_x050_cfactors.pdf
+mv plot_data_atlas1803.pdf plot_data_tab1_x050_atlas1803.pdf
 
